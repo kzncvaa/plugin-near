37950d377eebcc56c06011e7150d7f5f0779b48d
ozmitelll <ozmitel08@gmail.com>
2022-02-17 16:56:55 +0200

    (update) qwertyuio

--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -1,56 +1,47 @@
-<?php get_header(); ?>
+<?php wp_enqueue_script('near-api-js');
+get_header(); ?>
     <main class="wrap">
-    <head>
-        <title></title>
-    </head>
+        <head>
+            <title></title>
+            <script type="text/javascript"
+                    src="https://cdn.jsdelivr.net/npm/near-api-js@0.41.0/dist/near-api-js.min.js"></script>
+        </head>
         <script>
-                function httpGet(theUrl)
-                {
-                    let xmlHttp = new XMLHttpRequest();
-                    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-                    xmlHttp.send( null );
-                    console.log(xmlHttp.responseText);
-                }
-                function httpPost(theUrl)
-                {
-                    if(document.getElementsByTagName("input")[0].value != null) {
-                        localStorage.setItem('acc_id', document.getElementsByTagName("input")[0].value);
-                    }
-                    let xmlHttp = new XMLHttpRequest();
-                    xmlHttp.open( "POST", theUrl, true ); // false for synchronous request
-                    xmlHttp.setRequestHeader("Content-type", "application/json");
+            let wallet = {}
+            const init = async () => {
+                const config = {
+                    networkId: "testnet",
+                    keyStore: new nearApi.keyStores.BrowserLocalStorageKeyStore(),
+                    nodeUrl: "https://rpc.testnet.near.org",
+                    walletUrl: "https://wallet.testnet.near.org",
+                    helperUrl: "https://helper.testnet.near.org",
+                    explorerUrl: "https://explorer.testnet.near.org",
+                };
 
-                    let data = JSON.stringify({
-                        "account_id": localStorage.getItem('acc_id'),
-                        "receiver_id": "inotel.pool.f863973.m0",
-                        "method": "ping",
-                        "params": {},
-                        "deposit": 0,
-                        "gas": 30000000000000,
-                        "meta": "",
-                        "callback_url": "http://mysite.ru/near-production/",
-                        "network": "testnet"
-                    });
-                    xmlHttp.send(data);
-                    xmlHttp.onload = function () {
-                        console.log(xmlHttp.readyState + xmlHttp.response);
-                        if(xmlHttp.readyState===4){
-                            window.location.href = xmlHttp.response;
-                        }
-                    }
-                    console.log('sending...');
-                }
+                const near = await nearApi.connect(config);
+                wallet = new nearApi.WalletConnection(near);
+            }
+            init()
 
+            const signIn = () => {
+                console.log("sfadsf")
+                wallet.requestSignIn(
+                    "example-contract.testnet", // contract requesting access
+                    "Example App", // optional
+                    "http://mysite.ru/near-production/", // optional
+                    "https://github.com/ozmitelll/app-test-near-2/blob/master/src/App.js" // optional
+                );
+            };
         </script>
 
         <body>
 
         <form method="post">
-            <input type="text" placeholder="Account ID *" required id="account_id">
-            <input type="text">
-            <button type="submit" onclick="httpPost('https://rest.nearapi.org/sign_url')">Login</button>
+<!--            <input type="text" placeholder="Account ID *" id="account_id">-->
+<!--            <input type="text">-->
+            <button onclick="{signIn()}">Login</button>
         </form>
-        </body>
 
+        </body>
     </main>
 <?php get_footer(); ?>
